8a0d73b48d20fcf1e07556079397b1a15534db3f
Surya Pratap Singh <109263862+SuryaPS-06@users.noreply.github.com>
2026-02-09 11:13:02 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -5,7 +5,14 @@ for (let i = 1; i <= 10; i++) {
   <div class="product">
     <h3>Product ${i}</h3>
     <p>₹${i * 100}</p>
-    <button onclick="view(${i})">View</button>
+    <button
+  data-cta-name="product_view"
+  data-cta-location="plp_product_card"
+  data-product_id="product_${i}"
+  data-product_price="${i * 100}"
+  onclick="view(${i})">
+  View
+</button>
   </div>`;
 }
 
@@ -15,3 +22,4 @@ function view(id) {
   localStorage.setItem("pid", id);
   location.href = "pdp.html";
 }
+
